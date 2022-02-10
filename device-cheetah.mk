93cd6c132a4ad7a1fad9c0743942135367542ff0
Victor Liu <victorliu@google.com>
2022-02-10 13:52:05 -0800

    uwb: use per device factory calibration
    Use device specific script to select which factory calibration
    values to use.
    
    Bug: 218719905
    Test: on device
    Signed-off-by: Victor Liu <victorliu@google.com>
    Change-Id: Ie5adea6ce7b15c762a1c09a50b0a872465dcd3eb

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -29,7 +29,11 @@ include device/google/pantah/audio/cheetah/audio-tables.mk
 include device/google/gs201/device-shipping-common.mk
 include hardware/google/pixel/vibrator/cs40l26/device.mk
 include device/google/gs101/bluetooth/bluetooth.mk
-include device/google/gs201/uwb/uwb.mk
+
+ifeq ($(filter factory_cheetah, $(TARGET_PRODUCT)),)
+include device/google/gs101/uwb/uwb.mk
+include device/google/pantah/uwb/uwb_calibration.mk
+endif
 
 $(call soong_config_set,lyric,tuning_product,cheetah)
 $(call soong_config_set,google3a_config,target_device,cheetah)
