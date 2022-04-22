1f98950323e565e999a23f4264d5fed33d6c6a2e
Sayanna Chandula <sayanna@google.com>
2022-04-22 01:36:32 +0000

    thermal: enable thermal engine for evt1.0 devices
    New version of thermal engine (Powerlink v2) uses ODPM data for
    thermal mitigation. But proto devices does not support ODPM.
    Currently we have disabled thermal mitigation for all A0 based
    devices through a separate config, thermal_info_config_A0.json.
    This change restricts using separate thermal config  only for the
    proto devices and uses thermal_info_config.json for the rest.
    Fix a format issue with qi-quiet sensor definition.
    
    Bug: 196478280
    Test: Build and boot on device. Check thermal config
    Signed-off-by: Sayanna Chandula <sayanna@google.com>
    Change-Id: I0922e935cafe118f3437335e2a72b87aae6f6009

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -110,7 +110,7 @@ DEVICE_MANIFEST_FILE += \
 # Thermal Config
 PRODUCT_COPY_FILES += \
 	device/google/pantah/thermal_info_config_cheetah.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
-	device/google/pantah/thermal_info_config_A0.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_A0.json
+	device/google/pantah/thermal_info_config_proto.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_proto.json
 
 # Power HAL config
 PRODUCT_COPY_FILES += \
