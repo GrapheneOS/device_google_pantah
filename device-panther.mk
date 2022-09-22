0b95edea783c3daf1714a399eb10fc0382b9ca2c
shihchienc <shihchienc@google.com>
2022-09-22 07:02:44 +0000

    Change config path for LE Audio Capabilities file
    Since there is no different setting for sub project under same prohject,
    we will change the setting file from `bluetooth/project/` to
    `bluetooth/` to simplify the file structure.
    
    Bug: 238983662
    Test: make sure file added in config folder can be copied into devices
    Change-Id: I1b0d6a2107f5d69fd7bf650bad314e920810c156
    Merged-In: I1b0d6a2107f5d69fd7bf650bad314e920810c156
    (cherry picked from commit 6f30806b7829bed67896194f8bfe43dbcea21e31)

--- device-panther.mk
+++ device-panther.mk
@@ -278,7 +278,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # LE Auido Offload Capabilities setting
 PRODUCT_COPY_FILES += \
-    device/google/pantah/bluetooth/panther/le_audio_codec_capabilities.xml:$(TARGET_COPY_OUT_VENDOR)/etc/le_audio_codec_capabilities.xml
+    device/google/pantah/bluetooth/le_audio_codec_capabilities.xml:$(TARGET_COPY_OUT_VENDOR)/etc/le_audio_codec_capabilities.xml
 
 # Bluetooth EWP test tool
 PRODUCT_PACKAGES_DEBUG += \
