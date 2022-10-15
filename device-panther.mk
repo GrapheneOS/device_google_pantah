9f7ce292f4a4dea213fd6f01a8fc412a8373e51f
flawedworld <flawedworld@flawed.world>
2022-10-15 19:25:55 +0100

    Add sysconfig and permissions from stock OS
    Co-Authored-By: Vladislav <stvlad06@gmail.com>

--- device-panther.mk
+++ device-panther.mk
@@ -49,6 +49,10 @@ $(call soong_config_set,lyric,camera_hardware,panther)
 $(call soong_config_set,lyric,tuning_product,panther)
 $(call soong_config_set,google3a_config,target_device,panther)
 
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
+
 # Init files
 PRODUCT_COPY_FILES += \
 	device/google/pantah/conf/init.pantah.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.pantah.rc \
