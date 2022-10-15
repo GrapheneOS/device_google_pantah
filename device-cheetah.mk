bb1a389391f5a924d09eff057997db071591e1b6
flawedworld <flawedworld@flawed.world>
2022-10-15 19:25:55 +0100

    Add sysconfig and permissions from stock OS
    Co-Authored-By: Vladislav <stvlad06@gmail.com>

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -58,6 +58,10 @@ $(call soong_config_set,google3a_config,target_device,cheetah)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.support_kernel_idle_timer=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.ignore_hdr_camera_layers=true
 
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
+
 # Init files
 PRODUCT_COPY_FILES += \
 	device/google/pantah/conf/init.pantah.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.pantah.rc \
