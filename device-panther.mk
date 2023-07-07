7cd566d8d03b41cac9409d81ba8a7d2809ffc966
Vladislav <stvlad06@gmail.com>
2023-07-08 01:09:30 +0300

    commonize product-permissions-stock.xml [1/2]

--- device-panther.mk
+++ device-panther.mk
@@ -37,10 +37,9 @@ $(call soong_config_set,lyric,camera_hardware,panther)
 $(call soong_config_set,lyric,tuning_product,panther)
 $(call soong_config_set,google3a_config,target_device,panther)
 
-# sysconfig and permissions XML from stock
+# sysconfig XML from stock
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml \
-    $(LOCAL_PATH)/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
 
 # Init files
 PRODUCT_COPY_FILES += \
