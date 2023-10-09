2527249c2a591acd5f4f68a91aadee20ff438686
Roshan Pius <rpius@google.com>
2023-10-09 19:56:38 +0000

    pantah: Replace NFC app with NFC apex
    Bug: 303286040
    Test: Compiles
    (cherry picked from https://android-review.googlesource.com/q/commit:2811f6204de902a7b462078ffb23167351d572a3)
    Merged-In: Ifb1a5c4f685818fbfe816342e44487aa76528078
    Change-Id: Ifb1a5c4f685818fbfe816342e44487aa76528078

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -111,7 +111,7 @@ PRODUCT_COPY_FILES += \
 	device/google/pantah/nfc/libnfc-nci-cheetah.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	NfcNci \
+	com.android.nfcservices \
 	Tag \
 	android.hardware.nfc-service.st
 
--- device-ravenclaw.mk
+++ device-ravenclaw.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES += \
     device/google/pantah/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	NfcNci \
+	com.android.nfcservices \
 	Tag \
 	android.hardware.nfc-service.st
 
