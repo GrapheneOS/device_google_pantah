3000cdee0ef5284ba9fdc68bcdebedbcceb65b10
Roshan Pius <rpius@google.com>
2023-11-04 14:58:26 +0000

    Revert "pantah: Replace NFC app with NFC apex"
    Revert submission 24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Reason for revert: Causing bootloop with signed builds
    
    Reverted changes: /q/submissionid:24993894-cherrypicker-L80100000963393177:N49200001411943056
    
    Bug: 309117462
    Change-Id: I9adf4d5adf38a986fb95ca43e307914ae7439394
    Test: Compiles, device boots up

--- device-ravenclaw.mk
+++ device-ravenclaw.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES += \
     device/google/pantah/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	com.android.nfcservices \
+	NfcNci \
 	Tag \
 	android.hardware.nfc-service.st
 
