6c10bd721dab49692aa369452093c44c2f5804ed
Cyan_Hsieh <cyanhsieh@google.com>
2023-12-13 14:36:35 +0800

    Branch bootloader prebuilts for 24Q2 release
    Bug: 313350495
    Change-Id: Ia531c6512e8cef3c5d3081ee134b732232f1bc88

--- device-ravenclaw.mk
+++ device-ravenclaw.mk
@@ -147,6 +147,8 @@ PRODUCT_SOONG_NAMESPACES += \
 GOODIX_CONFIG_BUILD_VERSION := g6_trusty
 ifneq (,$(filter AP1%,$(RELEASE_PLATFORM_VERSION)))
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/firmware/fingerprint/24Q1
+else ifneq (,$(filter AP2%,$(RELEASE_PLATFORM_VERSION)))
+PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/firmware/fingerprint/24Q2
 else
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/firmware/fingerprint/trunk
 endif
@@ -168,6 +170,8 @@ PRODUCT_SOONG_NAMESPACES += device/google/pantah/cheetah/
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts
 ifneq (,$(filter AP1%,$(RELEASE_PLATFORM_VERSION)))
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/trusty/24Q1
+else ifneq (,$(filter AP2%,$(RELEASE_PLATFORM_VERSION)))
+PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/trusty/24Q2
 else
 PRODUCT_SOONG_NAMESPACES += vendor/google_devices/pantah/prebuilts/trusty/trunk
 endif
