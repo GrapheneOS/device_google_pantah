1e64b7ab90d2aa3131ef1b9dda0ba2cf87523d10
octocorvus <admin@octocorvus.dev>
2025-06-19 12:42:47 +0000

    device*.mk: remove insmod files
    handled by adevtool

--- device-ravenclaw.mk
+++ device-ravenclaw.mk
@@ -53,14 +53,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         device/google/pantah/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.ravenclaw.rc
 
-# insmod files. Kernel 5.10 prebuilts don't provide these yet, so provide our
-# own copy if they're not in the prebuilts.
-# TODO(b/369686096): drop this when 5.10 is gone.
-ifeq ($(wildcard $(TARGET_KERNEL_DIR)/init.insmod.*.cfg),)
-PRODUCT_COPY_FILES += \
-	device/google/pantah/init.insmod.ravenclaw.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.ravenclaw.cfg
-endif
-
 # Camera
 PRODUCT_COPY_FILES += \
 	device/google/pantah/media_profiles_ravenclaw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
