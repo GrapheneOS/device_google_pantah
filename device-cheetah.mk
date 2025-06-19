d05302b74d52fccee05e0ef0c6d337a4a031b7a6
octocorvus <admin@octocorvus.dev>
2025-06-19 14:06:22 +0000

    device*.mk: remove adevtool handled prop: persist.bluetooth.firmware.selection

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -102,9 +102,6 @@ DEVICE_MANIFEST_FILE += \
 	device/google/pantah/nfc/manifest_se.xml
 
 # Bluetooth HAL
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.firmware.selection=BCM.hcd
-
 # Bluetooth AAC VBR
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.a2dp_aac.vbr_supported=true
