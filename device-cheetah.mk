f582190f49fa4c07bc0d3a3842261896d6bc46b7
octocorvus <admin@octocorvus.dev>
2025-06-19 12:50:07 +0000

    device*.mk: nuke media profiles which are handled by adevtool

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -70,10 +70,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         device/google/pantah/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.cheetah.rc
 
-# Camera
-PRODUCT_COPY_FILES += \
-	device/google/pantah/media_profiles_cheetah.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
-
 # Media Performance Class 13
 PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=33
 
