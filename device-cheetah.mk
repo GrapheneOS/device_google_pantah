ad2183ddd987fa4341f7dc7a16ec4d69534360d7
octocorvus <admin@octocorvus.dev>
2025-06-19 12:35:05 +0000

    device*.mk: remove ro.surface_flinger.* product property overrides

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -57,9 +57,6 @@ $(call soong_config_set,lyric,camera_hardware,cheetah)
 $(call soong_config_set,lyric,tuning_product,cheetah)
 $(call soong_config_set,google3a_config,target_device,cheetah)
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.support_kernel_idle_timer=true
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.ignore_hdr_camera_layers=true
-
 # sysconfig XML from stock
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
