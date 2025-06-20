0db8da73632a3e4ee69185c6e805047497a52e28
octocorvus <admin@octocorvus.dev>
2025-06-20 00:42:02 +0000

    device*.xml: get rid SMR and EMR related props
    handled by adevtool

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -114,20 +114,6 @@ else
         device/google/pantah/location/gps_user.xml.c10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 endif
 
-# Set build properties for SMR builds
-ifeq ($(RELEASE_IS_SMR), true)
-    ifneq (,$(RELEASE_BASE_OS_CHEETAH))
-        PRODUCT_BASE_OS := $(RELEASE_BASE_OS_CHEETAH)
-    endif
-endif
-
-# Set build properties for EMR builds
-ifeq ($(RELEASE_IS_EMR), true)
-    ifneq (,$(RELEASE_BASE_OS_CHEETAH))
-        PRODUCT_PROPERTY_OVERRIDES += \
-        ro.build.version.emergency_base_os=$(RELEASE_BASE_OS_CHEETAH)
-    endif
-endif
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.gms.dck.eligible_wcc=3 \
--- device-panther.mk
+++ device-panther.mk
@@ -114,20 +114,6 @@ else
         device/google/pantah/location/gps_user.xml.p10:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 endif
 
-# Set build properties for SMR builds
-ifeq ($(RELEASE_IS_SMR), true)
-    ifneq (,$(RELEASE_BASE_OS_PANTHER))
-        PRODUCT_BASE_OS := $(RELEASE_BASE_OS_PANTHER)
-    endif
-endif
-
-# Set build properties for EMR builds
-ifeq ($(RELEASE_IS_EMR), true)
-    ifneq (,$(RELEASE_BASE_OS_PANTHER))
-        PRODUCT_PROPERTY_OVERRIDES += \
-        ro.build.version.emergency_base_os=$(RELEASE_BASE_OS_PANTHER)
-    endif
-endif
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.gms.dck.eligible_wcc=2 \
