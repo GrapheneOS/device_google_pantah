3bbb2e2e7b1a36ad2dab7a60e5e578b47001e038
octocorvus <admin@octocorvus.dev>
2025-06-20 02:32:48 +0000

    device*.mk: nuke adevtool handled prop: bluetooth.device_id.product_id

--- device-panther.mk
+++ device-panther.mk
@@ -131,11 +131,6 @@ ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Bluetooth device id
-# Panther: 0x4109
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.device_id.product_id=16649
-
 # ETM
 ifneq (,$(RELEASE_ETM_IN_USERDEBUG_ENG))
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
