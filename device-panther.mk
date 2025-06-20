9f789155c8dfc399dcf2c975941d00a1f205aa50
octocorvus <admin@octocorvus.dev>
2025-06-20 02:33:42 +0000

    device*.mk: nuke userdebug/eng ETM modules addition

--- device-panther.mk
+++ device-panther.mk
@@ -138,11 +138,4 @@ ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# ETM
-ifneq (,$(RELEASE_ETM_IN_USERDEBUG_ENG))
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-$(call inherit-product-if-exists, device/google/common/etm/device-userdebug-modules.mk)
-endif
-endif
-
 PRODUCT_VENDOR_PROPERTIES := $(filter-out ro.vendor.build.svn=% , $(PRODUCT_VENDOR_PROPERTIES))
