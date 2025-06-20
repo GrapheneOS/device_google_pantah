8be0eb8d36a164d432569b873471e8c20853a351
octocorvus <admin@octocorvus.dev>
2025-06-20 02:23:30 +0000

    device*.mk: nuke adevtool handled keyboard positioning props
    - ro.com.google.ime.kb_pad_port_b
    - ro.com.google.ime.kb_pad_port_l
    - ro.com.google.ime.kb_pad_port_r
    - ro.com.google.ime.height_ratio

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -137,13 +137,6 @@ $(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VE
 ACTUATOR_MODEL := luxshare_ict_081545
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 
-# Keyboard bottom and side padding in dp for portrait mode and height ratio
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.kb_pad_port_b=8 \
-    ro.com.google.ime.kb_pad_port_l=11 \
-    ro.com.google.ime.kb_pad_port_r=11 \
-    ro.com.google.ime.height_ratio=1.025
-
 # Enable camera exif model/make reporting
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.camera.exif_reveal_make_model=true \
