0825e68ac25ab87975f8df57787146a961f2604d
octocorvus <admin@octocorvus.dev>
2025-06-20 02:20:55 +0000

    device*.mk: remove adevtool handled prop: vendor.audio.hapticgenerator.distortion.output.gain

--- device-panther.mk
+++ device-panther.mk
@@ -134,10 +134,6 @@ $(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VE
 ACTUATOR_MODEL := luxshare_ict_081545
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 
-# Override Output Distortion Gain
-PRODUCT_VENDOR_PROPERTIES += \
-    vendor.audio.hapticgenerator.distortion.output.gain=0.38
-
 # Keyboard bottom padding in dp for portrait mode and height ratio
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_b=8 \
