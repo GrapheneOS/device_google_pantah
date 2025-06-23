bbfc768e02898d7375296705a81cf482b21a4bf9
octocorvus <admin@octocorvus.dev>
2025-06-23 13:12:10 +0000

    rm conf/init.pantah.rc
    let adevtool handle this

--- device-cheetah.mk
+++ device-cheetah.mk
@@ -59,7 +59,6 @@ $(call soong_config_set,google3a_config,target_device,cheetah)
 
 # Init files
 PRODUCT_COPY_FILES += \
-	device/google/pantah/conf/init.pantah.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.pantah.rc \
 	device/google/pantah/conf/init.cheetah.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.cheetah.rc
 
 # Recovery files
